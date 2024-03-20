cf3c3f8f2ca717234a8ba18e41b906a0b7bb395e
john7319 <jonniekaras@gmail.com>
2024-03-20 17:32:38 +0300

    Added the test functions and global variables

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+  
+function setBestCustomer() {
+    bestCustomer = 'not bob'; 
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+const leastFavoriteCustomer = "Samson"
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Joshua'; 
+    return leastFavoriteCustomer;
+}
